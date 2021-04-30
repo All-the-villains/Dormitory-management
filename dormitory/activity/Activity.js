d844b3c4192fd24a08f8da434c9cc22e78cd697a
星愿 <8065865+han-nah@user.noreply.gitee.com>
2021-04-30 15:12:51 +0800

    活动

--- dormitory/activity/Activity.js
+++ dormitory/activity/Activity.js
@@ -1,57 +1,39 @@
 import './Activity.css'
 import React, { Component } from 'react'
-import { Link,Switch } from 'react-router-dom'
-import {RouteWithSubRoutes} from '../../App'
-//import ActivityDetail from './ActivityDetail';
-import Value from './Value'
-
+import { Link } from 'react-router-dom'
 
 class Activity extends Component {
     constructor(){
         super();
         this.state={
-            data:[
-                {
-                    time:'04-09',text:"禁止在厕所抢吃抢喝",title:"禁止",period:"04-09至04-19"
-                },
-                {
-                    time:'04-12',text:"新风范，宿舍风采大赛"
-                },
-                {
-                    time:'04-13',text:"是谁偷了盛斌的袜子"
-                },
-                {
-                    time:'04-15',text:"可怜学生为何被别人的袜子熏晕在楼道" 
-                }
-            ],
+            data:[],
         }
     }
     onLoad(){
         this.props.history.push({ path : '/value' ,state : { key1: 'idx'} })
-
+        
     }    
-    // onChangeState(name){
-    //     this.setState(name)
-    // }
-    // componentDidMount(){
-    //     fetch('/text',{
-    //         method:'get',
-    //         mode:'cors',
-    //         headers:{'Content-Type':'application/json'},
-    //     }).then(res=>{
-    //         this.setState({data:res})
-    //     })
-    // }
+    componentDidMount(){
+        fetch('/activity',{
+            method:'get',
+            mode:'cors',
+            headers:{'Content-Type':'application/json'},
+        }).then(res=>res.json())
+        .then(res=>{
+            this.setState({data:res});
+            //console.log(res);
+            //console.log(this.state.data);
+        })
+    }
     render() {
         return(
             <div class="div1">
                 <h1>活动通知</h1>
-               {
+                {
                    this.state.data.map((val,idx)=>(
                        <div key={{idx}} class="div2">
-                           {/* {console.log(idx)} */}
-                           <span>活动日期：{val.time} </span>
-                           <Link to={{pathname:"/value",state:{key1:idx}}}>{val.text}</Link>
+                           <span>活动日期：{val.atime} </span>
+                           <Link to={{pathname:"/value",state:{key1:idx,data:this.state.data}}} style={{marginLeft:20}}>{val.atitle}</Link>
                        </div>
                     ))
                } 
--- dormitory/activity/Value.js
+++ dormitory/activity/Value.js
@@ -4,42 +4,23 @@ class Value extends Component {
     constructor() {
         super();
         this.state={
-            data:[
-                {
-                    time:'04-09',text:"禁止在厕所抢吃抢喝",title:"禁止",period:"04-09至04-19"
-                },
-                {
-                    time:'04-12',text:"新风范，宿舍风采大赛"
-                },
-                {
-                    time:'04-13',text:"是谁偷了盛斌的袜子"
-                },
-                {
-                    time:'04-15',text:"可怜学生为何被别人的袜子熏晕在楼道" 
-                }
-            ],
+            data:[],
         }
     }
-    
     componentWillMount(){
         //console.log(this.props.location)//传递过来的所有参数
         console.log(this.props.location.state.key1)
+        console.log(this.props.location.state.data)
+        this.state.data = this.props.location.state.data
+        console.log(this.state.data)
     }
-    //let id= this.props.location.state.key1;
-    // componentDidMount(){
-    //     fetch('/text',{
-    //         method:'get',
-    //         mode:'cors',
-    //         headers:{'Content-Type':'application/json'},
-    //     }).then(res=>{
-    //         this.setState({data:res})
-    //     })
-    // }
+    
     render() {
+        const id = this.props.location.state.key1
         return (
             <div class="div1">
-                <h1 style={{textAlign:'center'}}>{this.state.data[this.props.location.state.key1].title}</h1>
-                <span>{this.state.data[this.props.location.state.key1].text}</span>
+                <h1 style={{textAlign:'center'}}>{this.state.data[id].atitle}</h1>
+                <span>{this.state.data[id].atext}</span>
             </div>
         )
     }
