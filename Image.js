170153ef173ba4e85baab3970ba902fd092ba9dd
星愿 <8065865+han-nah@user.noreply.gitee.com>
2021-04-30 20:23:09 +0800

    第四次提交

--- Image.js
+++ Image.js
@@ -2,9 +2,9 @@ import React from 'react'
 import hebei from './title.png'
 const Image = () => {
     return (
-       <div style={{marginLeft:100,marginRight:100,borderTopWidth:3,borderTopColor:"blue",borderTopStyle:'solid'}}
+       <div style={{marginLeft:100,marginRight:100,borderTopWidth:3,borderTopColor:"blue",borderTopStyle:'solid',borderBottomWidth:2,borderBottomColor:"#ccc",borderBottomStyle:'solid'}}
        >
-           <img src={hebei} style={{borderBottomWidth:2,borderBottomColor:"#ccc",borderBottomStyle:'solid'}}></img>
+           <img src={hebei}></img>
        </div>
         
     ) 
